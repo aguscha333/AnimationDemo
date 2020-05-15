1bb51f6cd52455a552ec4e421179d58d9d3cb536
Agustina Chaer <aguschaer@gmail.com>
2020-05-15 10:28:18 -0300

    add main screens and navigation

---
+++ screens/MainScreen/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View} from 'react-native';
+
+import styles from './styles';
+
+const MainScreen = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.box} />
+    </View>
+  );
+};
+
+export default MainScreen;
